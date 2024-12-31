36c8b8774dda8c7e1af8fd70a4c989628802b1b5
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2024-12-31 23:01:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this._make = make;
+	this._model = model;
+	this.getMakeModel(){
+		return `${this._make} ${this._model}`;
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	call.Car(this,make,model);
+	this._topSpeed = topSpeed;
+	this.getTopSpeed(){
+		return `${this._topSpeed}`;
+	}
+	
+}
 
 // Do not change the code below
 window.Car = Car;
