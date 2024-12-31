bfac1e4f2edcbbacd9520399e8d5b96081d1bff8
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2024-12-31 23:15:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,19 @@
 function Car(make, model) {
 	this._make = make;
 	this._model = model;
-	this.getMakeModel(){
-		return `${this._make} ${this._model}`;
-	}
+}
+Car.prototype.getMakeModel = function(){
+	return `${this._make} ${this._model}`;
+}
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+SportsCar.prototype.getTopSpeed = function(){
+	return this._topSpeed;
 }
 
 function SportsCar(make, model, topSpeed) {
 	call.Car(this,make,model);
 	this._topSpeed = topSpeed;
-	this.getTopSpeed(){
-		return `${this._topSpeed}`;
 	}
 	
 }
